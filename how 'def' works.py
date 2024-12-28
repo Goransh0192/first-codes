b6d802104c9ee22f787af8f572e1a4ad27c3953a
Goransh0192 <goranshkedawat@gmail.com>
2024-12-28 17:46:13 +0530

    Delete how 'def' works.py

--- how 'def' works.py
+++
@@ -1,8 +0,0 @@
-# Define a function that adds two numbers
-def add_numbers(a, b):
-    result = a + b
-    return result
-
-# Call the function and print the result
-sum = add_numbers(3, 5)
-print(sum)  # Output: 8
